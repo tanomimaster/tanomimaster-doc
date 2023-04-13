a5d4324147e6450d5e6336b0ef2fac35c7668b97
Yuki Matsukura <matsubokkuri@gmail.com>
2023-04-13 16:38:42 +0900

    fix: edit link

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -24,13 +24,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/tanomimaster/tanomimaster-doc/edit/master/website/',
+          editUrl: 'https://github.com/tanomimaster/tanomimaster-doc/blob/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/tanomimaster/tanomimaster-doc/edit/master/website/blog/',
+            'https://github.com/tanomimaster/tanomimaster-doc/blob/develop/website/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
