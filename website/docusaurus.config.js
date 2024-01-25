b329f39c5372e866466bf11317e7b26656682380
Yuki Matsukura <matsubokkuri@gmail.com>
2024-01-25 17:02:34 +0900

    updated config.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,8 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
-
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -129,8 +127,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} Tanomimaster.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
       },
       zoom: {
         selector: '.markdown :not(em) > img',
